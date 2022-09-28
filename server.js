8d1b44d1119eead1f27152f38234ef252d6c51cc
Dennis Baksheev <dbaksheev@myseneca.ca>
2022-09-28 15:32:43 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
-const dataService = require("./data-service.js");
+const data_service = require('./data-sevice');
 const express = require('express');
 const path = require('path');
+const programs = require('./data/programs.json');
+const students = require('./data/students.json');
 const app = express();
 app.use(express.static("public"));
 const port = process.env.PORT || 8080;
@@ -11,25 +13,25 @@ app.get("/about", (req, res) => {
     res.sendFile(path.join(__dirname,"./views/about.html"));
 })
 
-app.get('/students', (req, res) => {
-  dataService.getAllStudents()
-      .then(data => res.json(data))
-      .catch(err => console.log(err))
+app.get('/students', async (req, res) => {
+  const allStudents = {...student.users}
+  res.json(allStudents)
+})
 
+app.get("/intlstudents", async (req, res) => {
+  const allStudents = { ...students.users.filter(user => user.isInternationalStudent === true) };
+  res.json(allStudents);
+});
+
+app.get("/programs", async (req, res) => {
+  const allPrograms = { ...programs.users };
+  res.json(allPrograms);
+});
+
+app.use((req, res) => {
+  res.status(404).send('Page Not Found')
 })
-app.get('/intlstudents', (req, res) => {
-  dataService.getIntlstudents()
-      .then(data => res.json(data))
-      .catch(err => console.log(err))
-})
-app.get('/programs', (req, res) => {
-  dataService.getPrograms()
-      .then(data => res.json(data))
-      .catch(err => console.log(err))
-})
-app.get('*', function (req, res) {
-  res.sendFile('./views/404.html', { root: __dirname })
-})
+
 
 app.listen(port, () => {
   console.log(`Express http server listening on ${port}`)});
