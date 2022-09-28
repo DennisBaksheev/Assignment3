25fd37f90b5a8523c1ad9f37ac83e1f95c7b6075
Dennis Baksheev <dbaksheev@myseneca.ca>
2022-09-28 15:39:29 -0400

    initial commit

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const students = require('./data/students.json');
 const app = express();
 app.use(express.static("public"));
 const port = process.env.PORT || 8080;
-app.get('/home', (req, res) => {
+app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname,"./views/home.html"));
 })
 app.get("/about", (req, res) => {
