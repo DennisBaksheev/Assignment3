9079d792f6ba3111a67cd9b05546caf0f9b82b0b
Dennis Baksheev <dbaksheev@myseneca.ca>
2022-09-28 21:56:32 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
-var data_service = require('./data-service')
+/*********************************************************************************
+*  WEB322 – Assignment 02
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source 
+*  (including 3rd party web sites) or distributed to other students.
+* 
+*  Name: _Dennis Baksheev_____________________ Student ID: _114797186_____________ Date: _2022-09-28_______________
+*
+*  Online (Cyclic) Link: 
+*
+********************************************************************************/ 
+
+
+
+
+
+
+var dataservice = require('./data-service')
 const express = require('express')
 const programs = require('./data/programs.json')
 const students = require('./data/students.json')
-const HTTP_PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080;
 const app = express()
 app.use(express.static('public')); 
 app.get('/', (req, res) => {
@@ -13,28 +29,39 @@ app.get('/about', (req, res) => {
     res.sendFile('./views/about.html', { root: __dirname })
 })
 
-app.get('/students', async (req, res) => {
-    // const allStudents = {...students.users}
-    res.json(allStudents)
+app.get('/students', (_, res) => {
+	dataservice.getAllStudents().then((data) => {
+		res.json(data)
+	}).catch((err) => {
+		res.json({ message: err })
+	})
 })
 
-app.get("/intlstudents", async (req, res) => {
-    // const allIntlstudents = { ...intlstudents.users.filter(user => user.isInternationalStudent === true) };
-    res.json(allIntlstudents);
-});
+app.get('/intlstudents', (_, res) => {
+	dataservice.getInternationalStudents().then((data) => {
+		res.json(data)
+	}).catch((err) => {
+		res.json({ message: err })
+	})
+})
 
-app.get("/programs", async (req, res) => {
-    // const allPrograms = { ...programs.users };
-    res.json(allPrograms);
-});
+app.get('/programs', (_, res) => {
+	dataservice.getPrograms().then((data) => {
+		res.json(data)
+	}).catch((err) => {
+		res.json({ message: err })
+	})
+})
 
 app.use((req, res) => {
     res.status(404).send('Page Not Found')
 })
 
-
-app.listen(HTTP_PORT, onHttpStart);
-
-function onHttpStart(){
-    console.log("Express http server listening on: " + HTTP_PORT);
-}
+dataservice.initialize().then(() => {
+	app.listen(PORT, () => {
+		console.log(`Express http server listening on ${PORT}`)
+	})
+})
+	.catch((err) => {
+		console.log(err)
+	})
