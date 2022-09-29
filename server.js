eb0ffff7272515bcee4bb189a6a9551433002f75
Dennis Baksheev <dbaksheev@myseneca.ca>
2022-09-28 20:08:58 -0400

    initial commit

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
+var data_service = require('./data-service')
 const express = require('express')
-const path = require('path')
-const data_service = require('./data-service.js')
+const programs = require('./data/programs.json')
+const students = require('./data/students.json')
+const HTTP_PORT = process.env.PORT || 8080;
 const app = express()
-var HTTP_PORT = process.env.PORT || 8080;
-
-app.use(express.static(path.join(__dirname, '/public')));
-
-app.get('/', (_, res) => {
-	res.sendFile(__dirname + '/views/home.html')
+app.use(express.static('public')); 
+app.get('/', (req, res) => {
+    res.sendFile('./views/home.html', { root: __dirname })
 })
 
-app.get('/about', (_, res) => {
-	res.sendFile(__dirname + '/views/about.html')
+app.get('/about', (req, res) => {
+    res.sendFile('./views/about.html', { root: __dirname })
 })
 
-app.get('*', (_, res) => {
-	res.status(404).send('Page Not Found')
+app.get('/students', async (req, res) => {
+    // const allStudents = {...students.users}
+    res.json(allStudents)
 })
 
-app.listen(HTTP_PORT);
+app.get("/intlstudents", async (req, res) => {
+    // const allIntlstudents = { ...intlstudents.users.filter(user => user.isInternationalStudent === true) };
+    res.json(allIntlstudents);
+});
+
+app.get("/programs", async (req, res) => {
+    // const allPrograms = { ...programs.users };
+    res.json(allPrograms);
+});
+
+app.use((req, res) => {
+    res.status(404).send('Page Not Found')
+})
+
+
+app.listen(HTTP_PORT, onHttpStart);
+
+function onHttpStart(){
+    console.log("Express http server listening on: " + HTTP_PORT);
+}
